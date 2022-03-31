b16d0c6eaf37cb18007e98be9887abe87f418e49
bright-jun <brightj529@gmail.com>
2022-03-31 10:18:49 +0900

    [Add] VarargsExample

--- src/playground/VarargsExample.java
+++ src/playground/VarargsExample.java
@@ -7,14 +7,14 @@ import java.util.List;
 public class VarargsExample {
 
     public static <T> boolean contains(T target, T... a) {
+        HashSet<T> hs;
         if (a == null) {
-            if(target == null){
-                return true;
-            }
-            return false;
+            hs = new HashSet<T>();
+            hs.add(null);
+        } else {
+            hs = new HashSet<T>(Arrays.asList(a));
         }
-
-        return new HashSet<T>(Arrays.asList(a)).contains(target);
+        return hs.contains(target);
     }
 
     public static void main(String[] args) {
@@ -24,15 +24,27 @@ public class VarargsExample {
         System.out.println(contains("C", null));
         System.out.println(contains("C", null, null));
         System.out.println(contains("C", null, null, "A"));
-
+        System.out.println("====================");
+        System.out.println(contains("A", Arrays.asList("A").toArray(new String[0])));
+        System.out.println(contains("A", Arrays.asList("A","B").toArray(new String[0])));
+        System.out.println(contains("A", Arrays.asList().toArray(new String[0])));
+//        System.out.println(contains("A", Arrays.asList(null).toArray(new String[0]))); // NPE
+        System.out.println(contains("A", Arrays.asList(null,"A").toArray(new String[0])));
+        System.out.println("====================");
         System.out.println(contains(null, "A"));
         System.out.println(contains(null, "A", "B"));
         System.out.println(contains(null, null));
         System.out.println(contains(null, null, null));
         System.out.println(contains(null, null, null, "A"));
+        System.out.println("====================");
+        System.out.println(contains(null, Arrays.asList("A").toArray(new String[0])));
+        System.out.println(contains(null, Arrays.asList("A","B").toArray(new String[0])));
+        System.out.println(contains(null, Arrays.asList().toArray(new String[0])));
+//        System.out.println(contains(null, Arrays.asList(null).toArray(new String[0]))); // NPE
+        System.out.println(contains(null, Arrays.asList(null,"A").toArray(new String[0])));
 
 //        List<String> a = Arrays.asList(null); // NPE
-
+        String nullStr = null;
         return;
     }
 }
